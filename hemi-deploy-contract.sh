1b42f7f559600a81697681f7f0eb5798443308fc
toanbm85 <94586777+ToanBm@users.noreply.github.com>
2024-09-20 13:58:32 +0700

    Create hemi-deploy-contract.sh

---
+++ hemi-deploy-contract.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+
+BOLD=$(tput bold)
+RESET=$(tput sgr0)
+YELLOW=$(tput setaf 3)
+# Logo
+
+echo     "*********************************************"
+echo     "Githuh: https://github.com/ToanBm"
+echo     "X: https://x.com/buiminhtoan1985"
+echo -e "\e[0m"
+
+print_command() {
+  echo -e "${BOLD}${YELLOW}$1${RESET}"
+}
+
+print_command "Updating System Packages..."
+npm init -y
+
+npm install --save-dev hardhat @nomiclabs/hardhat-ethers ethers @openzeppelin/contracts
+
+echo "4" | npx hardhat init
+
+mkdir contracts && mkdir scripts
+
+cat <<EOF > MyToken.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MyToken is ERC20 {
+    constructor(uint256 initialSupply) ERC20("MyToken", "MTK") {
+        _mint(msg.sender, initialSupply);
+    }
+}
+EOF
+
+npx hardhat compile
+
+npm install dotenv
+
+read -p "Enter your EVM wallet private key (without 0x): " PRIVATE_KEY
+
+print_command "Generating .env file..."
+cat <<EOF > .env
+PRIVATE_KEY=$PRIVATE_KEY
+EOF
+
+rm hardhat.config.js
+
+cat <<EOF > hardhat.config.js
+/** @type import('hardhat/config').HardhatUserConfig */
+require('dotenv').config()
+require("@nomiclabs/hardhat-ethers");
+
+module.exports = {
+  solidity: "0.8.20",
+  networks: {
+    hemi: {
+      url: "https://testnet.rpc.hemi.network/rpc",
+      chainId: 743111,
+      accounts: [`0x${process.env.PRIVATE_KEY}`],
+    },
+  }
+};
+EOF
+
+cat <<EOF > deploy.js
+const { ethers } = require("hardhat");
+
+async function main() {
+    const [deployer] = await ethers.getSigners();
+    const initialSupply = ethers.utils.parseUnits("1000", "ether");
+
+    const Token = await ethers.getContractFactory("MyToken");
+    const token = await Token.deploy(initialSupply);
+
+    console.log("Token deployed to:", token.address);
+}
+
+main().catch((error) => {
+    console.error(error);
+    process.exit(1);
+});
+EOF
+
+npx hardhat run scripts/deploy.js --network hemi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
